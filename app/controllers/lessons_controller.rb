0d97677c3d420b5b61b50857e27642eeb65337f3
Andi Setianto <andi.w.setianto@gmail.com>
2016-05-25 16:53:14 +0000

    Lesson 32 Styling the Instructor Course Overview Page

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,24 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
+
+
   def show
   end
 
   private
+
+  def require_authorized_for_current_lesson
+    if current_user.enrolled_in?(current_lesson.section.course)
+      return render text: 'Unauthorized', status: :unauthorized
+    end
+  end
+
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
 end
+
+
+
